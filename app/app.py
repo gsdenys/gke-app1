2041bf9dfb8b819892cde4fcfbb08bcc486eae2a
Denys G. Santos <gsdenys@gmail.com>
2021-04-05 12:42:45 -0300

    minimal app

--- app/app.py
+++ app/app.py
@@ -1,25 +1,14 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# # @app.route("/")
-# # def home():
-# #     return render_template("index.html")
-
-
 import flask
 
 app = flask.Flask(__name__)
-#app.config["DEBUG"] = True
 
+# @app.route('/', methods=['GET'])
+# def home():
+#     return "Applocation 1"
 
-@app.route('/', methods=['GET'])
-def home():
-    return "Applocation 1"
-
-@app.route('/test123', methods=['GET'])
+@app.route('/app1', methods=['GET'])
 def home1():
-    return "Applocation 1.1"
+    return "<h1>Applocation 1</h1>"
 
 if __name__ == "__main__":
 	app.run()
