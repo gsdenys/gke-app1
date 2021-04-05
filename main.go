7eed039b9cdcd7ec646a7dbe4fa762050175a92b
Denys G. Santos <gsdenys@gmail.com>
2021-04-04 21:52:07 -0300

    dockerfile change

--- main.go
+++ main.go
@@ -13,16 +13,16 @@ func html(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "index.html", nil)
 }
 
-// func css(w http.ResponseWriter, r *http.Request) {
-// 	templates.ExecuteTemplate(w, "index.css", nil)
-// }
+func css(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.css", nil)
+}
 
 func main() {
-	templates = template.Must(template.ParseGlob("templates/index.html"))
+	templates = template.Must(template.ParseGlob("templates/*"))
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", html).Methods("GET")
-	// r.HandleFunc("/css", css).Methods("GET")
+	r.HandleFunc("/css", css).Methods("GET")
 
 	http.Handle("/", r)
 	http.ListenAndServe(":3000", nil)
