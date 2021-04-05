8b738cb3fd985d0fbe8c4742f5b3e1f98ce34a3c
Denys G. Santos <gsdenys@gmail.com>
2021-04-05 13:07:21 -0300

    minimal app

--- app/app.py
+++ app/app.py
@@ -2,12 +2,13 @@ import flask
 
 app = flask.Flask(__name__)
 
-# @app.route('/', methods=['GET'])
-# def home():
-#     return "Applocation 1"
+
+@app.route('/', methods=['GET'])
+def health():
+    return ""
 
 @app.route('/app1', methods=['GET'])
-def home1():
+def home():
     return "<h1>Applocation 1!</h1>"
 
 if __name__ == "__main__":
